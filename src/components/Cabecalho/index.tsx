031cd0253b582ed5f96dd12e65f48b9d48665e10
Debora Souza <debora.souzalima22@gmail.com>
2023-10-20 20:16:01 -0300

    Alterando logo, nova imagem inserida na extensão svg, por ter uma melhor qualidade.

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,6 +1,6 @@
 import { Logo } from "./styles"
 
-import logo from '../../assets/logo.png'
+import logo from '../../assets/logo.svg'
 
 export function Cabecalho(){
   return (
--- src/components/Cabecalho/styles.ts
+++ src/components/Cabecalho/styles.ts
@@ -2,12 +2,14 @@ import styled from "styled-components";
 
 export const Cabecalho = styled.div`
   background-color: ${({ theme }) => theme.colors.lightPurple};
+
   font-size: 2.75rem;
   height: 4.50rem;
   padding-top: 0.5rem;
 `
 
 export const Logo = styled.img`
+  border: none;
   margin-left: 600px;
   height: 150px;
   width: 250px;
