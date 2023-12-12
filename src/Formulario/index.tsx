ec36a75e729694772ea5f02e9b788f4dc290ffd4
Debora Souza <debora.souzalima22@gmail.com>
2023-12-11 23:19:33 -0300

    Testando como estilizar componentes do Formulário.

--- src/Formulario/index.tsx
+++ src/Formulario/index.tsx
@@ -9,7 +9,7 @@ export function Formulario() {
     <FormularioTitulo>Agende já seu Horário</FormularioTitulo>
     <FormularioTextoInformativo>Informe seu nome e Telefone</FormularioTextoInformativo>
 
-    <form id="formulario">
+    <form>
       <label htmlFor="nome">Nome:</label>
       <input type="text" name="nome" id="nome" />
 
